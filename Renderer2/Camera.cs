7a818ded336a1dc063363bebb4c8bf8ab66f9523
billorer <zoran@suto.ro>
2019-04-25 22:25:17 +0200

    Added files

---
+++ Renderer2/Camera.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Renderer2
+{
+    public class Camera
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Target { get; set; }
+
+    }
+}
---
+++ Renderer2/Mesh.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderer2
+{
+    class Mesh
+    {
+        public string Name { get; set; }
+        public Vector3[] Vertices { get; private set; }
+       //  public Face[] Faces { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+
+        public Mesh(string name, int verticesCount)
+        {
+            Vertices = new Vector3[verticesCount];
+          //   Faces = new Face[facesCount];
+            Name = name;
+        }
+    }
+}
